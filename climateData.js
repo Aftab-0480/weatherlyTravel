ffbce0a52e6c92b75dc740ef849115a3a8cf00f6
Aftab-0480 <zeeshanamir927@gmail.com>
2025-07-18 12:16:20 +0530

    Add files via upload

---
+++ climateData.js
@@ -0,0 +1,22 @@
+export const climateData = {
+  "Manali": {
+    7: { temp: 20, humidity: 55, weather: "clear" },
+    12: { temp: 5, humidity: 40, weather: "snow" }
+  },
+  "Leh": {
+    7: { temp: 18, humidity: 30, weather: "clear" },
+    12: { temp: -2, humidity: 25, weather: "snow" }
+  },
+  "Agra": {
+    7: { temp: 35, humidity: 65, weather: "rain" },
+    12: { temp: 16, humidity: 55, weather: "clear" }
+  },
+  "Shillong": {
+    7: { temp: 23, humidity: 85, weather: "rain" },
+    12: { temp: 13, humidity: 65, weather: "cloudy" }
+  },
+  "Goa": {
+    7: { temp: 29, humidity: 90, weather: "rain" },
+    12: { temp: 26, humidity: 70, weather: "clear" }
+  }
+};
